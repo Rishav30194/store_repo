e0432bb81701bac3e828aae2ab5afe2b331eb389
rishav30194 <rishav30194@gmial.com>
2020-04-06 15:25:28 -0400

    updated files for heroku.

--- app.py
+++ app.py
@@ -7,17 +7,17 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from security import authenticate, identity
 
-application = Flask(__name__)
-application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-application.secret_key = 'rishav'
-api = Api(application)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'rishav'
+api = Api(app)
 
 @application.before_first_request
 def create_table():
     db.create_all()
 
-jwt = JWT(application, authenticate, identity) #/auth
+jwt = JWT(app, authenticate, identity) #/auth
 
 api.add_resource(Store,'/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>') #http://127.0.1.1:4999/student/Rolf
@@ -28,5 +28,5 @@ api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
     from db import db
-    db.init_app(application)
-    application.run(port=4999, debug=True)
+    db.init_app(app)
+    app.run(port=4999, debug=True)
